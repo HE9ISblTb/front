2654de4ceffb8651bd8d5137b43c0969c3b5db8f
maloy <antons99@mail.ru>
2022-04-08 13:21:57 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AutorizationComponent } from './components/autorization/autorization/autorization.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutorizationComponent
   ],
   imports: [
     BrowserModule
